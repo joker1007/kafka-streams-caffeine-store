4a000abe5f360ca96769e9e8bd088ed9a2907491
joker1007 <kakyoin.hierophant@gmail.com>
2022-05-01 19:25:00 +0900

    First implementation of CaffeineCachedKeyValueStore

--- lib/src/main/java/com/github/joker1007/kafka/streams/state/CaffeineCachedKeyValueStore.java
+++ lib/src/main/java/com/github/joker1007/kafka/streams/state/CaffeineCachedKeyValueStore.java
@@ -2,21 +2,37 @@ package com.github.joker1007.kafka.streams.state;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.NavigableSet;
 import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.concurrent.ConcurrentSkipListSet;
+import lombok.Builder;
+import lombok.Value;
+import org.apache.commons.collections4.iterators.PeekingIterator;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.processor.StateStore;
 import org.apache.kafka.streams.processor.StateStoreContext;
+import org.apache.kafka.streams.processor.internals.InternalProcessorContext;
+import org.apache.kafka.streams.processor.internals.ProcessorRecordContext;
 import org.apache.kafka.streams.state.KeyValueIterator;
 import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.internals.CacheFlushListener;
 import org.apache.kafka.streams.state.internals.WrappedStateStore;
 
 public class CaffeineCachedKeyValueStore<K extends Comparable<K>, V>
     extends WrappedStateStore<KeyValueStore<K, V>, K, V> implements KeyValueStore<K, V> {
   private final Caffeine<Object, Object> caffeine;
   private Cache<K, V> cache;
-  private SortedSet<K> cachedKeys;
+  private NavigableSet<K> cachedKeys;
+  private Map<K, DirtyEntry<V>> dirtyEntries;
+  private CacheFlushListener<K, V> cacheFlushListener;
+  private boolean sendOldValues;
+
+  private InternalProcessorContext context;
 
   public CaffeineCachedKeyValueStore(
       Caffeine<Object, Object> caffeine, KeyValueStore<K, V> wrapped) {
@@ -32,44 +48,131 @@ public class CaffeineCachedKeyValueStore<K extends Comparable<K>, V>
     return cache;
   }
 
-  private void putValueToCache(K key, V value) {
-    cache.put(key, value);
-    cachedKeys.add(key);
-  }
-
   @Override
   public void init(StateStoreContext context, StateStore root) {
     super.init(context, root);
+    if (context instanceof InternalProcessorContext) {
+      this.context = (InternalProcessorContext) context;
+    }
+    this.cachedKeys = new ConcurrentSkipListSet<>();
+    //noinspection SuspiciousMethodCalls
+    caffeine.evictionListener((key, value, cause) -> cachedKeys.remove(key));
     this.cache = caffeine.build();
-    this.cachedKeys = new TreeSet<>();
+    this.dirtyEntries = new HashMap<>();
+  }
+
+  @Value
+  @Builder(setterPrefix = "set")
+  public static class DirtyEntry<V> {
+    V newValue;
+    V oldValue;
+    Long timestamp;
+    Headers headers;
+    Integer partition;
+    String topic;
+    Long offset;
+
+    public ProcessorRecordContext recordContext() {
+      return new ProcessorRecordContext(timestamp, offset, partition, topic, headers);
+    }
   }
 
   @Override
   public void put(K key, V value) {
-    putValueToCache(key, value);
-    wrapped().put(key, value);
+    cache
+        .asMap()
+        .compute(
+            key,
+            (k, v) -> {
+              cachedKeys.add(k);
+              DirtyEntry.DirtyEntryBuilder<V> dirtyEntryBuilder =
+                  DirtyEntry.<V>builder().setNewValue(value).setOldValue(sendOldValues ? v : null);
+
+              if (context != null) {
+                dirtyEntryBuilder
+                    .setTimestamp(context.timestamp())
+                    .setHeaders(context.headers())
+                    .setPartition(context.partition())
+                    .setTopic(context.topic())
+                    .setOffset(context.offset());
+              }
+
+              dirtyEntries.put(k, dirtyEntryBuilder.build());
+              return value;
+            });
   }
 
   @Override
   public V putIfAbsent(K key, V value) {
-    V current = cache.getIfPresent(key);
-    if (current == null) {
-      putValueToCache(key, value);
-    }
-    return wrapped().putIfAbsent(key, value);
+    return cache
+        .asMap()
+        .computeIfAbsent(
+            key,
+            k -> {
+              cachedKeys.add(k);
+              DirtyEntry.DirtyEntryBuilder<V> dirtyEntryBuilder =
+                  DirtyEntry.<V>builder().setNewValue(value).setOldValue(null);
+
+              if (context != null) {
+                dirtyEntryBuilder
+                    .setTimestamp(context.timestamp())
+                    .setHeaders(context.headers())
+                    .setPartition(context.partition())
+                    .setTopic(context.topic())
+                    .setOffset(context.offset());
+              }
+              dirtyEntries.put(k, dirtyEntryBuilder.build());
+              return value;
+            });
   }
 
   @Override
   public void putAll(List<KeyValue<K, V>> entries) {
-    entries.forEach(keyValue -> putValueToCache(keyValue.key, keyValue.value));
-    wrapped().putAll(entries);
+    entries.forEach(
+        keyValue ->
+            cache
+                .asMap()
+                .compute(
+                    keyValue.key,
+                    (k, v) -> {
+                      cachedKeys.add(k);
+                      DirtyEntry.DirtyEntryBuilder<V> dirtyEntryBuilder =
+                          DirtyEntry.<V>builder()
+                              .setNewValue(keyValue.value)
+                              .setOldValue(sendOldValues ? v : null);
+
+                      if (context != null) {
+                        dirtyEntryBuilder
+                            .setTimestamp(context.timestamp())
+                            .setHeaders(context.headers())
+                            .setPartition(context.partition())
+                            .setTopic(context.topic())
+                            .setOffset(context.offset());
+                      }
+                      dirtyEntries.put(k, dirtyEntryBuilder.build());
+                      return keyValue.value;
+                    }));
   }
 
   @Override
   public V delete(K key) {
+    V oldValue = get(key);
     cache.invalidate(key);
     cachedKeys.remove(key);
-    return wrapped().delete(key);
+    DirtyEntry.DirtyEntryBuilder<V> dirtyEntryBuilder =
+        DirtyEntry.<V>builder().setNewValue(null).setOldValue(sendOldValues ? oldValue : null);
+
+    if (context != null) {
+      dirtyEntryBuilder
+          .setTimestamp(context.timestamp())
+          .setHeaders(context.headers())
+          .setPartition(context.partition())
+          .setTopic(context.topic())
+          .setOffset(context.offset());
+    }
+    dirtyEntries.put(key, dirtyEntryBuilder.build());
+
+    return oldValue;
   }
 
   @Override
@@ -77,23 +180,131 @@ public class CaffeineCachedKeyValueStore<K extends Comparable<K>, V>
     return cache.get(
         key,
         k -> {
-          cachedKeys.add(k);
-          return wrapped().get(k);
+          V value = wrapped().get(k);
+          if (value != null) {
+            cachedKeys.add(k);
+          }
+          return value;
         });
   }
 
   @Override
   public KeyValueIterator<K, V> range(K from, K to) {
-    return wrapped().range(from, to);
+    var keyRange = cachedKeys.subSet(from, true, to, true);
+    return new CacheEntryIterator<>(
+        cache, new PeekingIterator<>(keyRange.iterator()), wrapped().range(from, to), true);
+  }
+
+  @Override
+  public KeyValueIterator<K, V> reverseRange(K from, K to) {
+    var keyRange = cachedKeys.subSet(from, true, to, true);
+    return new CacheEntryIterator<>(
+        cache,
+        new PeekingIterator<>(keyRange.descendingIterator()),
+        wrapped().reverseRange(from, to),
+        false);
   }
 
   @Override
   public KeyValueIterator<K, V> all() {
-    return wrapped().all();
+    return new CacheEntryIterator<>(
+        cache, new PeekingIterator<>(cachedKeys.iterator()), wrapped().all(), true);
+  }
+
+  @Override
+  public KeyValueIterator<K, V> reverseAll() {
+    return new CacheEntryIterator<>(
+        cache,
+        new PeekingIterator<>(cachedKeys.descendingIterator()),
+        wrapped().reverseAll(),
+        false);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public <PS extends Serializer<P>, P> KeyValueIterator<K, V> prefixScan(
+      P prefix, PS prefixKeySerializer) {
+    var keyRange = cachedKeys.subSet((K) prefix, true, succKey(prefix), false);
+    return new CacheEntryIterator<>(
+        cache,
+        new PeekingIterator<>(keyRange.iterator()),
+        wrapped().prefixScan(prefix, prefixKeySerializer),
+        true);
+  }
+
+  @SuppressWarnings({"unchecked", "WrapperTypeMayBePrimitive"})
+  private <P> K succKey(P key) {
+    if (key instanceof String) {
+      String stringKey = (String) key;
+      var lastChar = stringKey.charAt(stringKey.length() - 1);
+      var nextChar = lastChar + 1;
+      var nextKey = stringKey.substring(0, stringKey.length() - 1) + Character.toString(nextChar);
+      return (K) nextKey;
+    } else if (key instanceof Integer) {
+      Integer next = (Integer) key + 1;
+      return (K) next;
+    } else if (key instanceof Long) {
+      Long next = (Long) key + 1;
+      return (K) next;
+    } else {
+      throw new UnsupportedOperationException("Given key type is not supported");
+    }
   }
 
   @Override
   public long approximateNumEntries() {
     return wrapped().approximateNumEntries();
+  }
+
+  @Override
+  public boolean setFlushListener(CacheFlushListener<K, V> listener, boolean sendOldValues) {
+    this.cacheFlushListener = listener;
+    this.sendOldValues = sendOldValues;
+    return super.setFlushListener(listener, sendOldValues);
+  }
+
+  @Override
+  public void flushCache() {
+    doFlush();
+    super.flushCache();
+  }
+
+  @Override
+  public void flush() {
+    doFlush();
+    super.flush();
+  }
+
+  private void doFlush() {
+    dirtyEntries.forEach(
+        (k, v) -> {
+          writeInner(k, v.newValue);
+
+          if (cacheFlushListener != null) {
+            if (context != null) {
+              var current = context.recordContext();
+              context.setRecordContext(v.recordContext());
+              try {
+                cacheFlushListener.apply(
+                    k,
+                    v.newValue,
+                    v.oldValue,
+                    v.timestamp != null ? v.timestamp : System.currentTimeMillis());
+              } finally {
+                context.setRecordContext(current);
+              }
+            } else {
+              cacheFlushListener.apply(k, v.newValue, v.oldValue, System.currentTimeMillis());
+            }
+          }
+        });
+  }
+
+  private void writeInner(K key, V value) {
+    if (value != null) {
+      wrapped().put(key, value);
+    } else {
+      wrapped().delete(key);
+    }
   }
 }
--- lib/src/test/java/com/github/joker1007/kafka/streams/state/CaffeineCachedKeyValueStoreTest.java
+++ lib/src/test/java/com/github/joker1007/kafka/streams/state/CaffeineCachedKeyValueStoreTest.java
@@ -3,20 +3,28 @@ package com.github.joker1007.kafka.streams.state;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.processor.api.MockProcessorContext;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.Stores;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CaffeineCachedKeyValueStoreTest {
-  private MockProcessorContext<String, String> context;
+
+  private KeyValueStore<String, String> innerStore;
   private CaffeineCachedKeyValueStore<String, String> caffeineCachedStore;
 
   @BeforeEach
   void setUp() {
-    this.context = new MockProcessorContext<>();
-    var store =
+    MockProcessorContext<String, String> context = new MockProcessorContext<>();
+    innerStore =
         Stores.keyValueStoreBuilder(
                 Stores.inMemoryKeyValueStore("inmemory-key-value-store"),
                 Serdes.String(),
@@ -25,18 +33,245 @@ class CaffeineCachedKeyValueStoreTest {
             .withLoggingDisabled()
             .build();
 
-    var caffeine = Caffeine.newBuilder().maximumSize(100);
-    caffeineCachedStore = new CaffeineCachedKeyValueStore<>(caffeine, store);
+    var caffeine = Caffeine.newBuilder().maximumSize(10);
+    caffeineCachedStore = new CaffeineCachedKeyValueStore<>(caffeine, innerStore);
     context.addStateStore(caffeineCachedStore);
     caffeineCachedStore.init(context.getStateStoreContext(), caffeineCachedStore);
+  }
+
+  private <K extends Comparable<K>, V> void assertCaching(
+      CaffeineCachedKeyValueStore<K, V> caffeineCachedStore, K key, V value) {
+    assertEquals(value, caffeineCachedStore.get(key));
+    assertEquals(value, caffeineCachedStore.getCache().getIfPresent(key));
+    assertEquals(value, caffeineCachedStore.wrapped().get(key));
+    assertTrue(caffeineCachedStore.getCachedKeys().contains(key));
   }
 
   @Test
   void testPut() {
     caffeineCachedStore.put("foo", "bar");
+    caffeineCachedStore.flush();
+    assertCaching(caffeineCachedStore, "foo", "bar");
+  }
+
+  @Test
+  void testPutIfAbsent() {
+    var result = caffeineCachedStore.putIfAbsent("foo", "bar");
+    caffeineCachedStore.flush();
+    assertEquals("bar", result);
+    assertCaching(caffeineCachedStore, "foo", "bar");
+
+    result = caffeineCachedStore.putIfAbsent("foo", "hoge");
+    assertEquals("bar", result);
+    assertCaching(caffeineCachedStore, "foo", "bar");
+  }
+
+  @Test
+  void testPutAll() {
+    var entries =
+        List.of(
+            KeyValue.pair("foo1", "bar1"),
+            KeyValue.pair("foo2", "bar2"),
+            KeyValue.pair("foo3", "bar3"));
+    caffeineCachedStore.putAll(entries);
+    caffeineCachedStore.flush();
+
+    assertCaching(caffeineCachedStore, "foo1", "bar1");
+    assertCaching(caffeineCachedStore, "foo2", "bar2");
+    assertCaching(caffeineCachedStore, "foo3", "bar3");
+  }
+
+  @Test
+  void testDelete() {
+    caffeineCachedStore.put("foo", "bar");
+    caffeineCachedStore.flush();
+    caffeineCachedStore.delete("foo");
+    caffeineCachedStore.flush();
+    assertNull(caffeineCachedStore.get("foo"));
+    assertFalse(caffeineCachedStore.getCachedKeys().contains("foo"));
+  }
+
+  @Test
+  void testGetWithoutCachedValue() {
+    assertNull(caffeineCachedStore.get("foo"));
+    assertTrue(caffeineCachedStore.getCachedKeys().isEmpty());
+    innerStore.put("foo", "bar");
+
     assertEquals("bar", caffeineCachedStore.get("foo"));
-    assertEquals("bar", caffeineCachedStore.getCache().getIfPresent("foo"));
-    assertEquals("bar", caffeineCachedStore.wrapped().get("foo"));
-    assertTrue(caffeineCachedStore.getCachedKeys().contains("foo"));
+    assertCaching(caffeineCachedStore, "foo", "bar");
+  }
+
+  private void setUpIteratorTest() {
+    var entries =
+        List.of(
+            KeyValue.pair("foo1", "bar1"),
+            KeyValue.pair("foo2", "bar2"),
+            KeyValue.pair("foo6", "bar6"));
+    caffeineCachedStore.putAll(entries);
+    caffeineCachedStore.flush();
+
+    caffeineCachedStore.put("foo7", "bar7");
+
+    var entriesOnlyInnerStore =
+        List.of(KeyValue.pair("foo3", "bar3"), KeyValue.pair("foo4", "bar4"));
+    innerStore.putAll(entriesOnlyInnerStore);
+  }
+
+  @Test
+  void testAll() {
+    setUpIteratorTest();
+
+    try (var it = caffeineCachedStore.all()) {
+      List<KeyValue<String, String>> list = new ArrayList<>();
+      it.forEachRemaining(list::add);
+      assertEquals(6, list.size());
+      assertEquals("bar1", list.get(0).value);
+      assertEquals("bar2", list.get(1).value);
+      assertEquals("bar3", list.get(2).value);
+      assertEquals("bar4", list.get(3).value);
+      assertEquals("bar6", list.get(4).value);
+      assertEquals("bar7", list.get(5).value);
+    }
+  }
+
+  @Test
+  void testRange() {
+    setUpIteratorTest();
+
+    try (var it = caffeineCachedStore.range("foo2", "foo4")) {
+      List<KeyValue<String, String>> list = new ArrayList<>();
+      it.forEachRemaining(list::add);
+      assertEquals(3, list.size());
+      assertEquals("bar2", list.get(0).value);
+      assertEquals("bar3", list.get(1).value);
+      assertEquals("bar4", list.get(2).value);
+    }
+  }
+
+  @Test
+  void testReverseAll() {
+    setUpIteratorTest();
+
+    try (var it = caffeineCachedStore.reverseAll()) {
+      List<KeyValue<String, String>> list = new ArrayList<>();
+      it.forEachRemaining(list::add);
+      assertEquals(6, list.size());
+      assertEquals("bar7", list.get(0).value);
+      assertEquals("bar6", list.get(1).value);
+      assertEquals("bar4", list.get(2).value);
+      assertEquals("bar3", list.get(3).value);
+      assertEquals("bar2", list.get(4).value);
+      assertEquals("bar1", list.get(5).value);
+    }
+  }
+
+  @Test
+  void testReverseRange() {
+    setUpIteratorTest();
+
+    try (var it = caffeineCachedStore.reverseRange("foo2", "foo4")) {
+      List<KeyValue<String, String>> list = new ArrayList<>();
+      it.forEachRemaining(list::add);
+      assertEquals(3, list.size());
+      assertEquals("bar4", list.get(0).value);
+      assertEquals("bar3", list.get(1).value);
+      assertEquals("bar2", list.get(2).value);
+    }
+  }
+
+  @Test
+  void testPrefixScan() {
+    setUpIteratorTest();
+    caffeineCachedStore.put("foo21", "bar21");
+    caffeineCachedStore.put("foo22", "bar22");
+    caffeineCachedStore.put("foo2_1", "bar2_1");
+    innerStore.put("foo21", "bar21");
+    innerStore.put("foo2_2", "bar2_2");
+
+    try (var it = caffeineCachedStore.prefixScan("foo2", Serdes.String().serializer())) {
+      List<KeyValue<String, String>> list = new ArrayList<>();
+      it.forEachRemaining(list::add);
+      assertEquals(5, list.size());
+      assertEquals("bar2", list.get(0).value);
+      assertEquals("bar21", list.get(1).value);
+      assertEquals("bar22", list.get(2).value);
+      assertEquals("bar2_1", list.get(3).value);
+      assertEquals("bar2_2", list.get(4).value);
+    }
+  }
+
+  @Test
+  void testEviction() {
+    IntStream.rangeClosed(1, 100)
+        .forEach(
+            i -> {
+              String key = "foo" + String.format("%03d", i);
+              String value = "bar" + String.format("%03d", i);
+              caffeineCachedStore.put(key, value);
+            });
+    caffeineCachedStore.flush();
+
+    List<KeyValue<String, String>> list = new ArrayList<>();
+    caffeineCachedStore.getCache().asMap().forEach((k, v) -> list.add(KeyValue.pair(k, v)));
+    assertTrue(list.size() < 100);
+    list.clear();
+
+    try (var it = caffeineCachedStore.all()) {
+      it.forEachRemaining(list::add);
+    }
+
+    assertEquals(100, list.size());
+
+    IntStream.rangeClosed(1, 100)
+        .forEach(
+            i -> {
+              String key = "foo" + String.format("%03d", i);
+              String value = "bar" + String.format("%03d", i);
+              assertEquals(key, list.get(i - 1).key);
+              assertEquals(value, list.get(i - 1).value);
+            });
+  }
+
+  @Test
+  void testFlushListener() {
+    Map<String, String> newValues = new HashMap<>();
+    caffeineCachedStore.setFlushListener(
+        (key, newValue, oldValue, timestamp) -> {
+          newValues.put(key, newValue);
+        },
+        false);
+
+    caffeineCachedStore.put("foo1", "bar1");
+    caffeineCachedStore.put("foo2", "bar2");
+    caffeineCachedStore.put("foo1", "bar1_2");
+
+    caffeineCachedStore.flush();
+
+    assertEquals("bar1_2", newValues.get("foo1"));
+    assertEquals("bar2", newValues.get("foo2"));
+  }
+
+  @Test
+  void testFlushListenerWithOldValue() {
+    Map<String, List<String>> newValues = new HashMap<>();
+    caffeineCachedStore.setFlushListener(
+        (key, newValue, oldValue, timestamp) -> {
+          List<String> list = new ArrayList<>();
+          list.add(newValue);
+          list.add(oldValue);
+          newValues.put(key, list);
+        },
+        true);
+
+    caffeineCachedStore.put("foo1", "bar1");
+    caffeineCachedStore.put("foo2", "bar2");
+    caffeineCachedStore.put("foo1", "bar1_2");
+
+    caffeineCachedStore.flush();
+
+    assertEquals("bar1_2", newValues.get("foo1").get(0));
+    assertEquals("bar1", newValues.get("foo1").get(1));
+    assertEquals("bar2", newValues.get("foo2").get(0));
+    assertNull(newValues.get("foo2").get(1));
   }
 }
